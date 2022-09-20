6476a34abd077f8a3fdcb35b8693515709c8ecbd
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-09-20 12:24:22 -0700

    Update primaryroads.sql

--- Assignments/P02/primaryroads.sql
+++ Assignments/P02/primaryroads.sql
@@ -3,3 +3,5 @@
 -- shp2pgsql -s 4326 -I /Users/parkerhagmaier/Desktop/BBBB/tl_2019_us_primaryroads/tl_2019_us_primaryroads.shp primaryroads| psql -d project02 -U parkerhagmaier
 
 CREATE INDEX idx_primaryroads_geom ON public.primaryroads USING GIST (the_geom);
+
+SELECT * FROM public.primaryroads LIMIT 5;
