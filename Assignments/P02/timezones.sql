ae7e166de2c0e00ea47ad2660e856e0a4a0261d7
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-09-20 12:10:26 -0700

    Update timezones.sql

--- Assignments/P02/timezones.sql
+++ Assignments/P02/timezones.sql
@@ -1,5 +1,5 @@
 --Using the command line I did the following to create the table 
 
--- shp2pgsql -s 4326 -I INPUTSHAPEFILE.shp TABLENAME| psql -d DATABASENAME -U USERNAME
+-- shp2pgsql -s 4326 -I /Users/parkerhagmaier/Desktop/BBBB/ne_10m_time_zones/ne_10m_time_zones.shp timezones| psql -d project02 -U parkerhagmaier
 
 CREATE INDEX idx_timezones_geom ON public.timezones USING GIST (the_geom);
