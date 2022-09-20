e778163a954399c05a4ca1d728ab35d71e880b3f
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-09-20 12:14:14 -0700

    Create primaryroads.sql

---
+++ Assignments/P02/primaryroads.sql
@@ -0,0 +1,5 @@
+--Using the command line I did the following to create the table 
+
+-- shp2pgsql -s 4326 -I /Users/parkerhagmaier/Desktop/BBBB/tl_2019_us_primaryroads/tl_2019_us_primaryroads.shp primaryroads| psql -d project02 -U parkerhagmaier
+
+CREATE INDEX idx_primaryroads_geom ON public.primaryroads USING GIST (the_geom);
