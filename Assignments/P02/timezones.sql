b299dca31e8309cc17cf92b8470e8890860ec9ad
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-09-20 12:23:26 -0700

    Update timezones.sql

--- Assignments/P02/timezones.sql
+++ Assignments/P02/timezones.sql
@@ -3,3 +3,5 @@
 -- shp2pgsql -s 4326 -I /Users/parkerhagmaier/Desktop/BBBB/ne_10m_time_zones/ne_10m_time_zones.shp timezones| psql -d project02 -U parkerhagmaier
 
 CREATE INDEX idx_timezones_geom ON public.timezones USING GIST (the_geom);
+
+SELECT * FROM public.timezones LIMIT 5;
