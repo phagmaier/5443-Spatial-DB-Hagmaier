bb2775375960893ffa57534ac6e9a57981a5278f
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-09-20 12:24:37 -0700

    Update militarybases.sql

--- Assignments/P02/militarybases.sql
+++ Assignments/P02/militarybases.sql
@@ -3,3 +3,5 @@
 -- shp2pgsql -s 4326 -I /Users/parkerhagmaier/Desktop/BBBB/tl_2021_us_mil/tl_2021_us_mil.shp militarybases| psql -d project02 -U parkerhagmaier
 
 CREATE INDEX idx_militarybases_geom ON public.militarybases USING GIST (the_geom);
+
+SELECT * FROM public.militarybases LIMIT 5;
